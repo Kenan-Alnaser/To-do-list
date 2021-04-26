d1d8082a2216340578c880f4afe5f780f4f8dd20
Kenan Alnaser <kenan-na@live.com>
2021-04-26 11:59:53 +0200

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const addToList = () => {
+  let userData = document.querySelector("#userData").value;
+  if (userData != "") {
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(userData);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userData").value = "";
+  } else {
+    document.querySelector("#userData").placeholder = "Please Enter text first";
+  }
+};
